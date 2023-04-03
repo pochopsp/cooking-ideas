f33bde29555116b4090c0b1ffa1a77f81b8fb666
Lorenzo Bugiani <lorenzo.bugiani@gmail.com>
2023-04-03 11:39:49 +0200

    Return bad request for IllegalIdException

--- src/main/java/com/github/cookingideas/application/controller/RecipeController.java
+++ src/main/java/com/github/cookingideas/application/controller/RecipeController.java
@@ -2,11 +2,10 @@ package com.github.cookingideas.application.controller;
 
 import com.github.cookingideas.application.dto.RecipeDTO;
 import com.github.cookingideas.domain.entity.Recipe;
+import com.github.cookingideas.domain.exception.IllegalIdException;
 import com.github.cookingideas.domain.usecase.GetRecipeById;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("recipe")
@@ -23,5 +22,10 @@ public class RecipeController {
         return getRecipeById.execute(new Recipe.Id(id))
             .map(RecipeDTO::from)
             .orElse(null);
+    }
+
+    @ExceptionHandler(IllegalIdException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public void handleException(IllegalIdException e) {
     }
 }
--- src/test/java/com/github/cookingideas/acceptance/RecipeControllerTest.java
+++ src/test/java/com/github/cookingideas/acceptance/RecipeControllerTest.java
@@ -1,7 +1,9 @@
 package com.github.cookingideas.acceptance;
 
 import io.restassured.RestAssured;
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,7 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 import static io.restassured.RestAssured.get;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-import static org.springframework.http.HttpStatus.OK;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class RecipeControllerTest {
@@ -23,11 +24,19 @@ public class RecipeControllerTest {
     }
 
     @Test
+    @DisplayName("Return a recipe")
     public void greetingShouldReturnDefaultMessage() {
         // TODO: We should first insert and then assert that we get the inserted recipe,
         // not rely on data already on the db
         get("/recipe/1").then()
-            .statusCode(OK.value())
+            .statusCode(HttpStatus.SC_OK)
             .assertThat().body(matchesJsonSchemaInClasspath("responses/recipe-schema.json"));
+    }
+
+    @Test
+    @DisplayName("Return bad request if the id is not valid")
+    public void badRequest() {
+        get("/recipe/0").then()
+            .statusCode(HttpStatus.SC_BAD_REQUEST);
     }
 }
